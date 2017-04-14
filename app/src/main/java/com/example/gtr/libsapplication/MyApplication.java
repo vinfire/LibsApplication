babbf4a1594bf7a9f6c691d6ced978442aeb8ce3
Vinfire <vinfire4th@163.com>
2017-04-14 11:12:46 +0800

    add tinker

--- app/src/main/java/com/example/gtr/libsapplication/MyApplication.java
+++ app/src/main/java/com/example/gtr/libsapplication/MyApplication.java
@@ -3,11 +3,17 @@ package com.example.gtr.libsapplication;
 import android.app.Application;
 import android.content.Context;
 
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
 /**
  * Created by GTR on 2017/4/4.
  */
-
-public class MyApplication extends Application {
+@DefaultLifeCycle(application = "com.example.gtr.libsapplication.MyApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class MyApplication extends DefaultApplicationLike {
 
     private static Context mContext;
 
